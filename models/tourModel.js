7004b267c01ff81a9e791a8bfbc0f8531952f40b
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-24 02:58:51 +0530

    Modelling Tour

--- models/tourModel.js
+++ models/tourModel.js
@@ -32,7 +32,25 @@ const tourSchema = new mongoose.Schema({
     required: [true, 'A tour must have a price'],
   },
   discount: Number,
-  summary: { type: String, trim: true },
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a summary'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
