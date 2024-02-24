4fb2735137c4cc713ab83b0d0ddc7134396dc2e3
yyppsk <69805496+yyppsk@users.noreply.github.com>
2024-02-25 02:27:04 +0530

    Update Env

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
+
+dotenv.config({ path: './conf.env' });
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
